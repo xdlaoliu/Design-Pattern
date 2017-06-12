f0efb2bcb0ef40c02c31e2bf50037a96c820dc20
xdlaoliu <zhengguangjiexd@126.com>
2017-06-12 08:28:04 +0800

    Fixed Builder Derector Issue

--- src/com/jason/designpattern/builder/Main.java
+++ src/com/jason/designpattern/builder/Main.java
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String[] args){
         Director director = new Director();
         BenzModel benzModel = director.getBenzModel();
-        BMWModel bmwModel = director.getBMWModel();
         benzModel.run();
+        BMWModel bmwModel = director.getBMWModel();
         bmwModel.run();
     }
 
